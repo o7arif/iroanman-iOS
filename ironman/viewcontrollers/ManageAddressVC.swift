6673940d7cd32e19d82e7fcef3ac74e4330870b7
Md Arif <arif991846@gmail.com>
2021-07-28 22:13:26 +0600

    manage address vc

---
+++ ironman/viewcontrollers/ManageAddressVC.swift
@@ -0,0 +1,150 @@
+//
+//  ManageAddress.swift
+//  ironman
+//
+//  Created by Md Nazmul Haque Arif on 7/28/21.
+//
+
+import UIKit
+
+class ManageAddressVC: UIViewController {
+    private let container = UIView()
+    
+    override func viewDidLoad() {
+        setupViews()
+    }
+    
+    private func setupViews() {
+        self.view.addSubview(container)
+        container.backgroundColor = .color(fromHexString: "FAFAFA")
+        container.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        container.addSubview(viewHeaderBack)
+        viewHeaderBack.snp.makeConstraints { make in
+            make.left.top.right.equalToSuperview()
+            make.height.equalTo(100)
+        }
+        
+        viewHeaderBack.addSubview(ivBack)
+        ivBack.snp.makeConstraints { make in
+            make.left.equalToSuperview().inset(20)
+            make.bottom.equalToSuperview().inset(16)
+        }
+        
+        viewHeaderBack.addSubview(labelHeaderTitle)
+        labelHeaderTitle.snp.makeConstraints { make in
+            make.bottom.equalTo(ivBack.snp.bottom)
+            make.top.equalTo(ivBack.snp.top)
+            make.left.right.equalToSuperview()
+        }
+        
+        authentionRequiredMessage()
+    }
+    
+    
+    private func authentionRequiredMessage() {
+        let authRequiredContainer = UIView()
+        container.addSubview(authRequiredContainer)
+        authRequiredContainer.snp.makeConstraints { make in
+            make.centerY.equalToSuperview()
+            make.left.right.equalToSuperview().inset(46)
+        }
+        
+        authRequiredContainer.addSubview(ivAddress)
+        ivAddress.snp.makeConstraints { make in
+            make.left.top.right.equalToSuperview()
+        }
+        
+        authRequiredContainer.addSubview(labelNotAuthenticated)
+        labelNotAuthenticated.snp.makeConstraints { make in
+            make.top.equalTo(ivAddress.snp.bottom).offset(24)
+            make.left.right.bottom.equalToSuperview()
+        }
+        
+        container.addSubview(btnSignIn)
+        btnSignIn.snp.makeConstraints { make in
+            make.left.right.equalToSuperview().inset(30)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-20)
+            make.height.equalTo(50)
+        }
+        
+    }
+    
+    
+    // MARK: CLICK ACTIONS
+    
+    @objc private func signInTapped(_ sender: Any) {
+        print("sign in tapped")
+    }
+    
+    
+    
+    // MARK: ATTRIBUTE INITIALIZATION
+    
+    private let ivBack: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "ic_back")
+        imageView.contentMode = .scaleAspectFit
+        imageView.isUserInteractionEnabled = true
+        return imageView
+    }()
+    
+    private let viewHeaderBack: UIView = {
+        let view = UIView()
+        view.backgroundColor = .primary
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 20
+        view.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        return view
+    }()
+    
+    private let labelHeaderTitle: UILabel = {
+        let label = UILabel()
+        label.font = OpenSans.semiBold.of(size: 20)     // should be Poppins Semibold 20
+        label.numberOfLines = 1
+        label.textAlignment = .center
+        label.textColor = .white
+        label.text = "Manage Address"
+        return label
+    }()
+    
+    private let ivAddress: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "img_address")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let labelNotAuthenticated: UILabel = {
+        let label = UILabel()
+        label.font = OpenSans.bold.of(size: 30)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.textColor = .textBlack
+        label.text = "You are not signed in!\nPlease sign in first."
+        return label
+    }()
+    
+    private let btnSignIn: UIView = {
+        let button = UIButton()
+        button.setTitle("Sign in", for: .normal)
+        button.isUserInteractionEnabled = true
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = .primary
+        button.titleLabel?.font = OpenSans.bold.of(size: 15)
+        button.clipsToBounds = true
+        button.layer.cornerRadius = 25
+        button.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner, .layerMinXMaxYCorner, .layerMaxXMaxYCorner]
+        button.layer.shadowColor = UIColor.color(fromHexString: "808082").cgColor
+        button.layer.shadowOffset = CGSize(width: 1, height: 2)
+        button.layer.shadowOpacity = 0.4
+        button.layer.shadowRadius = 10
+        button.layer.masksToBounds = false
+        
+        // click action
+        button.addTarget(self, action: #selector(signInTapped(_:)), for: .touchUpInside)
+        return button
+    }()
+}
