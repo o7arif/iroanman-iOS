e8fef7b6632c1afe1cc4fdf049934bf0e62e9254
Md Arif <arif991846@gmail.com>
2021-11-21 20:44:03 +0600

    user model changes regarding backend changes

--- ironman/models/auth/User.swift
+++ ironman/models/auth/User.swift
@@ -14,6 +14,12 @@ class User : NSObject, NSCoding {
     var email : String
     var mobile : String
     var profilePhoto : String
+    var firstName : String
+    var lastName : String
+    var gender : String
+    var mobileVerifiedAt : String
+    var alternativePhone : String
+    var isActive : Bool
     
     override init() {
         self.id = 0
@@ -21,6 +27,12 @@ class User : NSObject, NSCoding {
         self.email = ""
         self.mobile = ""
         self.profilePhoto = ""
+        self.firstName = ""
+        self.lastName = ""
+        self.gender = ""
+        self.mobileVerifiedAt = ""
+        self.alternativePhone = ""
+        self.isActive = false
     }
     
     convenience init(fromDictionary dictionary: [String:Any]?) {
@@ -30,6 +42,12 @@ class User : NSObject, NSCoding {
         email = dictionary?["email"] as? String ?? ""
         mobile = dictionary?["mobile"] as? String ?? ""
         profilePhoto = dictionary?["profile_photo_path"] as? String ?? ""
+        firstName = dictionary?["first_name"] as? String ?? ""
+        lastName = dictionary?["last_name"] as? String ?? ""
+        gender = dictionary?["gender"] as? String ?? ""
+        mobileVerifiedAt = dictionary?["mobile_verified_at"] as? String ?? ""
+        alternativePhone = dictionary?["alternative_phone"] as? String ?? ""
+        isActive = dictionary?["is_active"] as? Bool ?? false
     }
     
     internal func encode(with aCoder: NSCoder) {
@@ -38,6 +56,12 @@ class User : NSObject, NSCoding {
         aCoder.encode(self.email, forKey: "email")
         aCoder.encode(self.mobile, forKey: "mobile")
         aCoder.encode(self.profilePhoto, forKey: "profile_photo_path")
+        aCoder.encode(self.firstName, forKey: "first_name")
+        aCoder.encode(self.lastName, forKey: "last_name")
+        aCoder.encode(self.gender, forKey: "gender")
+        aCoder.encode(self.mobileVerifiedAt, forKey: "mobile_verified_at")
+        aCoder.encode(self.alternativePhone, forKey: "alternative_phone")
+        aCoder.encode(self.isActive, forKey: "is_active")
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -46,6 +70,12 @@ class User : NSObject, NSCoding {
         self.email = aDecoder.decodeObject(forKey: "email") as? String ?? ""
         self.mobile = aDecoder.decodeObject(forKey: "mobile") as? String ?? ""
         self.profilePhoto = aDecoder.decodeObject(forKey: "profile_photo_path") as? String ?? ""
+        self.firstName = aDecoder.decodeObject(forKey: "first_name") as? String ?? ""
+        self.lastName = aDecoder.decodeObject(forKey: "last_name") as? String ?? ""
+        self.gender = aDecoder.decodeObject(forKey: "gender") as? String ?? ""
+        self.mobileVerifiedAt = aDecoder.decodeObject(forKey: "mobile_verified_at") as? String ?? ""
+        self.alternativePhone = aDecoder.decodeObject(forKey: "alternative_phone") as? String ?? ""
+        self.isActive = aDecoder.decodeBool(forKey: "is_active") as Bool
     }
 
 }
